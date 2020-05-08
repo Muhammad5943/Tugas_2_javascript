c4cb28d3825dbc4589132f8f58c6c8ccabf21de6
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-05-08 20:19:55 +0700

    tugas2js
    Mengumpulkan tugas 2 js

---
+++ tugas2.js
@@ -0,0 +1,21 @@
+var string = "Aku";
+var number = 25;
+var boolean = false;
+var numboolean = 50;
+var undefined;
+var array = [45,"aku",77,"dia",98,"kita"]
+
+if(boolean >= 45)
+numboolean = true;
+
+else
+numboolean = false;
+
+console.log(numboolean);
+console.log(string);
+console.log(number);
+console.log(array);
+console.log(undefined);
+
+
+
